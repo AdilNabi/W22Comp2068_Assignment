dd29d438d36350b39b2270826a03d3e6ebf44dca
AdilNabi <77406332+AdilNabi@users.noreply.github.com>
2022-04-20 02:32:21 +0500

    Created basic update and read functionality. Users can upload Images

--- imageApp/test-app/imageApp/test-app/src/render.js
+++
@@ -1,9 +0,0 @@
-let form = document.getElementById('form')
-
-form.addEventListener('submit',function(event) {
-    event.preventDefault()
-    var search = document.getElementById('search').value
-    var url = "https://source.unsplash.com/random/300×200/?" + search
-    var img = document.getElementById('img')
-    img.src = url
-})
