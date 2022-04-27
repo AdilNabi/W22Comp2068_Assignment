2720fa8db9dfd1ad2f2b52609b99a4c7cd3b0b05
AdilNabi <77406332+AdilNabi@users.noreply.github.com>
2022-04-27 19:52:11 +0500

    Git hub commit errors fixed double code

--- lab2/server.js
+++ lab2/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const connect = require('connect');
 const url = require('url');
 const app = connect();
@@ -42,49 +41,4 @@ function calculator(request,response,next) {
     }
     response.end();
 }
-=======
-const connect = require('connect');
-const url = require('url');
-const app = connect();
-
-app.listen(3000);
-
-console.log('Server is running');
-
-function calculator(request,response,next) {
-
-    response.writeHead(200, 'text-html');
-
-    let queryString = url.parse(request.url, true).query;
-    let method = queryString.method;
-    let x = queryString.x;
-    let y = queryString.y;
-    let result;
-
-    if (method === 'add')
-    {
-        result = parseFloat(x) + parseFloat(y);
-        response.write(`<h1>Output: ${x} + ${y} = ${result} </h1>`);
-    }
-
-    else if (method === 'subtract')
-    {
-        result = parseFloat(x) - parseFloat(y);
-        response.write(`<h1>Output: ${x} - ${y} = ${result} </h1>`);
-    }
-
-    else if (method === 'multiply')
-    {
-        result = parseFloat(x) * parseFloat(y);
-        response.write(`<h1>Output: ${x} * ${y} = ${result} </h1>`);
-    }
-
-    else if (method === 'divide')
-    {
-        result = parseFloat(x) / parseFloat(y);
-        response.write(`<h1>Output: ${x} / ${y} = ${result} </h1>`);
-    }
-    response.end();
-}
->>>>>>> f4fa9c76c0b7e951ac3cd16acc6f35ac6f0a3fd6
 app.use(calculator);
